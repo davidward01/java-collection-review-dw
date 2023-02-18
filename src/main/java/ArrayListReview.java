916b4489b439c27280e829fc5101c86177d7f2c1
davidward01 <davidward.info@gmail.com>
2023-02-18 16:14:24 -0500

    added comparator classes to ArrayListReview

--- src/main/java/ArrayListReview.java
+++ src/main/java/ArrayListReview.java
@@ -13,7 +13,7 @@ public class ArrayListReview {
 
         System.out.println(students);
 
-        System.out.println("Printing with for i loop");
+        System.out.println("Printing with for i loop...");
 
         // iteration on ArrayLists
 
@@ -26,14 +26,14 @@ public class ArrayListReview {
         Iterator iter = students.listIterator();
 
         // forward iteration
-        System.out.println("Printing with forward Iterator");
+        System.out.println("Printing with forward Iterator...");
 
         while (iter.hasNext()) {
             System.out.println(iter.next());
         }
 
         // backwards iteration
-        System.out.println("Printing with backwards Iterator");
+        System.out.println("Printing with backwards Iterator...");
 
         while (((ListIterator<?>) iter).hasPrevious()) {
             System.out.println(((ListIterator<?>) iter).previous());
@@ -41,18 +41,44 @@ public class ArrayListReview {
 
 
         // 3. for each loop (enhanced for loop)
-        System.out.println("Printing with for each loop");
+        System.out.println("Printing with for each loop...");
 
         for (Student student : students) {
             System.out.println(student);
         }
 
         // 4. lambda expression
-        System.out.println("Printing with lambda expression");
+        System.out.println("Printing with lambda expression...");
         students.forEach(student -> System.out.println(student));
 //        students.forEach(System.out::println);
 
 
+        // Sorting elements in List
+        System.out.println("Printing with sorted list by comp...");
+        Collections.sort(students, new sortByIdDesc());
+        System.out.println(students);
+
+        System.out.println("Printing with sorted list by name desc...");
+        Collections.sort(students, new sortByIdNameDesc());
+        System.out.println(students);
+
+
+
     }
+
+    static class sortByIdDesc implements Comparator<Student>{
+        @Override
+        public int compare(Student o1, Student o2){
+            return o2.id-o1.id;
+        }
+    }
+
+    static class sortByIdNameDesc implements Comparator<Student>{
+        @Override
+        public int compare(Student o1, Student o2){
+            return o2.name.compareToIgnoreCase(o1.name);
+        }
+    }
+
 
 }
